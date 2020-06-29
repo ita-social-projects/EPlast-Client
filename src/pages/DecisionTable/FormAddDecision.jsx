59da6e3a1b252de2937e2710a7a0aa165f760b43
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-06-29 14:21:46 +0300

    added select field to organization and target

--- src/pages/DecisionTable/FormAddDecision.jsx
+++ src/pages/DecisionTable/FormAddDecision.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Form, DatePicker, Select, Input, Upload, Button } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
 const FormAddDecision = () => {
+  useEffect(() => {
 
+  });
   const normFile = e => {
     // console.log('Upload event:', e);
 
@@ -26,7 +28,7 @@ const FormAddDecision = () => {
     >
       <Form.Item
         label="Назва рішення"
-        name="username"
+        name="name"
         rules={[
           {
             message: 'Це поле має бути заповненим',
@@ -36,15 +38,23 @@ const FormAddDecision = () => {
         <Input />
       </Form.Item>
 
-      <Form.Item label="Рішення органу">
+      <Form.Item
+        label="Рішення органу"
+        name = "organization">
         <Select>
-          <Select.Option value="1">Demo 1</Select.Option>
-          <Select.Option value="2">Demo 2</Select.Option>
-          <Select.Option value="3">Demo 3</Select.Option>
+          <Select.Option value="0">КПР</Select.Option>
+          <Select.Option value="1">КРК</Select.Option>
+          <Select.Option value="2">КБ УПС</Select.Option>
+          <Select.Option value="3">КБ УСП</Select.Option>
+          <Select.Option value="4">КБ УПЮ</Select.Option>
+          <Select.Option value="5">КБ УПН</Select.Option>
+          <Select.Option value="6">КБ УПП</Select.Option>
         </Select>
       </Form.Item>
 
-      <Form.Item label="Тематика рішення">
+      <Form.Item
+       label="Тематика рішення"
+       name = "target">
         <Select>
           <Select.Option value="1">Demo 1</Select.Option>
           <Select.Option value="2">Demo 2</Select.Option>
@@ -57,6 +67,13 @@ const FormAddDecision = () => {
       <Form.Item label="Текст рішення">
         <Input.TextArea allowClear />
       </Form.Item>
+      <Form.Item label="Статус рішення">
+        <Select>
+          <Select.Option value="0">У розгляді</Select.Option>
+          <Select.Option value="1">Підтверджено</Select.Option>
+          <Select.Option value="2">Скасовано</Select.Option>
+        </Select>
+        </Form.Item>
       <Form.Item label="Прикріпити">
         <Form.Item name="dragger" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
           <Upload.Dragger name="files" action="/upload.do">
