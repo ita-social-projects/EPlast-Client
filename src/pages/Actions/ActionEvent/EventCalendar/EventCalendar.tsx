f666088f3e945364bc7dec329d5bac7229f016bb
Шутяк Остап <ishutyak2@gmail.com>
2020-08-04 16:42:37 +0300

    Added event detali after clicking on them in calendar

--- src/pages/Actions/ActionEvent/EventCalendar/EventCalendar.tsx
+++ src/pages/Actions/ActionEvent/EventCalendar/EventCalendar.tsx
@@ -1,39 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import eventUserApi from '../../../../api/eventUserApi';
 import { Button, Space, Spin, Modal } from 'antd';
-import FullCalendar, { CalendarData, CalendarApi } from '@fullcalendar/react'
+import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import listPlugin from '@fullcalendar/list'
 import ukLocale from '@fullcalendar/core/locales/uk';
-import { calendarFormat } from 'moment';
+import moment from 'moment';
+import { useHistory } from 'react-router-dom';
 
 const classes = require('./EventCalendar.module.css');
 
 export default function () {
 
     const [loading, setLoading] = useState(false);
-
     const [actions, setActions] = useState<any>([]);
     const [educations, setEducations] = useState<any>([]);
     const [camps, setCamps] = useState<any>([]);
-
     const [eventModal, setEventModal] = useState(false);
-
     const eventsColors: string[] = ['#6f8ab5', '#fdcb02', '#c01111'];
-
-    function getConcatedEvents(): Array<any> {
-        (actions as Array<any>).forEach(event => {
-            Object.assign(event, { color: eventsColors[0] })
-        });
-        (educations as Array<any>).forEach(event => {
-            Object.assign(event, { color: eventsColors[1] })
-        });
-        (camps as Array<any>).forEach(event => {
-            Object.assign(event, { color: eventsColors[2] })
-        });
-
-        return (actions as Array<any>).concat(educations as Array<any>).concat(camps as Array<any>);
-    }
+    const [eventInfo, setEventInfo] = useState<any>();
+    const history = useHistory();
 
     useEffect(() => {
         const fetchData = async () => {
@@ -53,13 +39,26 @@ export default function () {
         fetchData();
     }, []);
 
+    function getConcatedEvents(): Array<any> {
+        (actions as Array<any>).forEach(event => {
+            Object.assign(event, { color: eventsColors[0] })
+        });
+        (educations as Array<any>).forEach(event => {
+            Object.assign(event, { color: eventsColors[1] })
+        });
+        (camps as Array<any>).forEach(event => {
+            Object.assign(event, { color: eventsColors[2] })
+        });
 
-    function handleEventClick(event: any) {
-        console.log(event);
-        setEventModal(true);
+        return (actions as Array<any>).concat(educations as Array<any>).concat(camps as Array<any>);
     }
 
 
+
+    const handleEventClick = (clickInfo: any) => {
+        setEventModal(true);
+        setEventInfo(clickInfo);
+    }
 
     return loading === false ? (
         <div className={classes.spaceWrapper}>
@@ -124,6 +123,14 @@ export default function () {
                                 </Button>
                             ]}
                     >
+                        <h1>{eventInfo?.event?.title}</h1>
+                        <h2>Дата початку: {moment(eventInfo?.event?.start).format("DD-MM-YYYY HH:mm")}</h2>
+                        <h2>Дата завершення: {moment(eventInfo?.event?.end).format("DD-MM-YYYY HH:mm")}</h2>
+                        <h2>Локація: {eventInfo?.event?._def.extendedProps.eventlocation}</h2>
+                        <h2>Опис: {eventInfo?.event?._def.extendedProps.description}</h2>
+                        < Button type="primary" className={classes.button} id={classes.button} onClick={() => history.push(`/events/${eventInfo?.event?.id}/details`)} >
+                            Деталі
+                        </Button>
                     </Modal >
                 </div>
             </div>
