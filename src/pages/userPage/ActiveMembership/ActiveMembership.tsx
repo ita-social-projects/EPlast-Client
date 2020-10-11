068042b79629f038111f8ce7e449e0742a8a6879
mlabensky <mlabensky1998@gmail.com>
2020-10-12 02:05:57 +0300

    Added GeneralInfo, added button to UserTable

--- src/pages/userPage/ActiveMembership/ActiveMembership.tsx
+++ src/pages/userPage/ActiveMembership/ActiveMembership.tsx
@@ -2,13 +2,17 @@ import React,{useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import classes from './ActiveMembership.module.css'
 import { Avatar, Typography, List, Button } from 'antd';
-import activeMembershipApi, { UserPlastDegree } from '../../../api/activeMembershipApi';
+
+import '../personalData/PersonalData.less';
+import activeMembershipApi, { UserDates, UserPlastDegree } from '../../../api/activeMembershipApi';
 import userApi from '../../../api/UserApi';
 import AuthStore from '../../../stores/AuthStore';
 import jwt from 'jwt-decode';
 import ModalAddPlastDegree from './PlastDegree/ModalAddPlastDegree';
 import moment from 'moment';
+import AvatarAndProgress from '../personalData/AvatarAndProgress';
 import ModalAddEndDatePlastDegree from './PlastDegree/ModalAddEndDatePlastDegree';
+import ModalChangeUserDates from './UserDates/ModalChangeUserDates';
 import DeleteDegreeConfirm from './PlastDegree/DeleteDegreeConfirm';
 import { SafetyCertificateOutlined } from '@ant-design/icons';
 
@@ -16,11 +20,14 @@ const { Title } = Typography;
 
 const ActiveMembership = () => {
     const { userId } = useParams();
-    const [imageBase64, setImageBase64] = useState<string>();
+    const [data, setData] = useState<any>({});
     const [accessLevels,setAccessLevels ] = useState([]);
+    const [dates, setDates] = useState<any>({});
     const [user, setUser] = useState<any>({});
+    const [LoadInfo, setLoadInfo] = useState<boolean>(false);
     const [plastDegrees, setPlastDegrees] = useState<Array<UserPlastDegree>>([]);
     const [visibleModal, setVisibleModal] = useState<boolean>(false);
+    const [datesVisibleModal, setDatesVisibleModal] = useState<boolean>(false);
     const [userToken, setUserToken] = useState<any>([{ nameid: '' }]);
     const [endDateVisibleModal,setEndDateVisibleModal] = useState<boolean>(false);
     const [plastDegreeIdToAddEndDate, setPlastDegreeIdToAddEndDate] = useState<number>(0);
@@ -58,18 +65,28 @@ const ActiveMembership = () => {
     const fetchData  =  async () =>{
         const token = AuthStore.getToken() as string;
             setUserToken(jwt(token));
-         
-        setAccessLevels(await activeMembershipApi.getAccessLevelById(userId));
+
         await userApi.getById(userId).then(async response => {
             setUser(response.data.user)
-           
-            await userApi.getImage(response.data.user.imagePath).then((response: { data: any; }) => {
-                setImageBase64(response.data);
-            })
         });
-       await activeMembershipApi.getUserPlastDegrees(userId).then(response =>{
+
+        setAccessLevels(await activeMembershipApi.getAccessLevelById(userId));
+
+        await activeMembershipApi.getUserDates(userId).then(response =>{
+            response.dateEntry = response.dateEntry === "0001-01-01T00:00:00" ? "" : response.dateEntry
+            response.dateOath = response.dateOath === "0001-01-01T00:00:00" ? "" : response.dateOath
+            response.dateEnd = response.dateEnd === "0001-01-01T00:00:00" ? "" : response.dateEnd
+            setDates(response)
+            setLoadInfo(true)
+        });
+
+        await activeMembershipApi.getUserPlastDegrees(userId).then(response =>{
            setPlastDegrees(response);
-       });
+        });
+
+        await userApi.getImage(user.imagePath).then((response: { data: any; }) => {
+            setData(response.data);
+        })
     };
     const IsUserHasAnyAdminTypeRoles = (userRoles: Array<string>): boolean => {
         let IsUserHasAnyAdminRole = false;
@@ -88,34 +105,70 @@ const ActiveMembership = () => {
     const handleAddEndDate = async () =>{
         fetchData();
     }
+    const handleChangeDates = async () =>{
+        
+        fetchData();
+    }
     const showModal = () => setVisibleModal(true);
     useEffect(()=>{
         fetchData();
     },[]);
 return <div className={classes.wrapper} >
-                <div className={classes.wrapperImg}>
-                    <Avatar size={250} src={imageBase64} />
-                    <Title level={2}> {user.firstName} {user.lastName} </Title>
-               < div className={classes.line} id={classes.line} />
-               {IsUserHasAnyAdminTypeRoles(userToken["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"])
+
+        <div className={classes.avatarWrapper}>
+            <AvatarAndProgress 
+            imageUrl={user.imagePath} 
+            time={348}
+                // data.timeToJoinPlast} 
+            firstName={user.firstName} 
+            lastName={user.lastName} 
+            isUserPlastun={true}
+            // data.isUserPlastun}
+            />
+
+            {IsUserHasAnyAdminTypeRoles(userToken["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"])
                 &&
                 <Button type="primary" onClick={showModal}>
                 Додати ступінь
               </Button>
                }
-                </div>
+        </div>
 
         <div className={classes.wrapperCol} >
         <div className={classes.wrapper}>
         <div className={classes.wrapper2}>
+        <Title level={2}> Загальна інформація </Title> 
+        <div className={classes.line} />
+            {
+                LoadInfo ? ( 
+                <>
+                    <div className={classes.textGeneralInfo}>
+                        Дата вступу: { dates?.dateEntry === "" ? 'Не задано' : moment(dates.dateEntry).format("DD-MM-YYYY")}
+                    </div>
+                    <div className={classes.textGeneralInfo}>
+                        Дата присяги: {dates?.dateOath === "" ? 'Без присяги' : moment(dates.dateOath).format("DD-MM-YYYY") } 
+                    </div>
+                    <div className={classes.textGeneralInfo}>
+                        Дата завершення: {dates?.dateEnd === "" ? 'Ще в Пласті' : moment(dates.dateEnd).format("DD-MM-YYYY") } 
+                    </div>
+                    {IsUserHasAnyAdminTypeRoles(userToken["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"])
+                    &&
+                    <Button type="primary"className={classes.buttonChange}  onClick={() => {setDatesVisibleModal(true)}}>
+                        Змінити
+                    </Button>
+                    }
+                 </>
+                ) :
+                <div></div>
+            }
+        </div>
 
-             </div>
-             <div className={classes.wrapper2}> 
-        <Title level={2}> Рівні доступу користувача </Title>      
+        <div className={classes.wrapper2}> 
+        <Title level={2}> Рівні доступу </Title>      
              <div className={classes.line} />
             <List
             dataSource = {accessLevels}
-            renderItem={item => <List.Item style ={ {fontSize : "20px"}}>{item}</List.Item> } />
+            renderItem={item => <List.Item style ={ {fontSize : "16px"}}>{item}</List.Item> } />
            
         </div>
         </div>
@@ -175,6 +228,12 @@ return <div className={classes.wrapper} >
         endDateVisibleModal = {endDateVisibleModal}
         setEndDateVisibleModal = {setEndDateVisibleModal}
         handleAddEndDate = {handleAddEndDate}/>
-</div>;
+        <ModalChangeUserDates 
+        userId = {userId}
+        dates = {dates}
+        datesVisibleModal = {datesVisibleModal}
+        setDatesVisibleModal = {setDatesVisibleModal}
+        handleChangeDates = {handleChangeDates}/>
+</div>
 }
 export default ActiveMembership;
