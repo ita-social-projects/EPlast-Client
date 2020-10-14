1891c0fea89320bee045a651febdfdb6e98e9663
DESKTOP-1VNOUHA\Yurii <yuriy.shyshkun@gmail.com>
2020-10-14 14:19:52 +0300

    created delete and download buttons

--- src/pages/userPage/Blanks/UserAchievements/ListOfAchievementsModal.tsx
+++ src/pages/userPage/Blanks/UserAchievements/ListOfAchievementsModal.tsx
@@ -1,13 +1,17 @@
-import { FileTextOutlined } from "@ant-design/icons";
+import { DeleteOutlined, DownloadOutlined, FileTextOutlined } from "@ant-design/icons";
 import { List, Modal } from "antd";
-import React from "react";
+import React, { useEffect } from "react";
+import { getAchievementFile, removeAchievementDocument } from "../../../../api/blankApi";
 import BlankDocument from "../../../../models/Blank/BlankDocument";
 import classes from "./ListOfAchievements.module.css"
+import notificationLogic from '../../../../components/Notifications/Notification';
+
 
 interface Props {
     visibleModal: boolean;
     setVisibleModal: (visibleModal: boolean) => void;
     achievementDoc: BlankDocument[];
+    setAchievementDoc: (document: BlankDocument[]) => void;
 }
 
 const ListOfAchievementsModal = (props: Props) => {
@@ -16,8 +20,19 @@ const ListOfAchievementsModal = (props: Props) => {
         props.setVisibleModal(false);
     }
 
+    const deleteFIle = async (documentId: number, fileName:string) => {
+        await removeAchievementDocument(documentId);
+        notificationLogic('success', `Файл "${fileName}" успішно видалено`);
+        props.setAchievementDoc(props.achievementDoc.filter((d) => d.id !== documentId));
+    }
+
+    const downloadFile = async (fileBlob: string, fileName: string) => {
+        await getAchievementFile(fileBlob,fileName);
+    }
+
     return (
         <Modal
+            title="Список досягнень"
             visible={props.visibleModal}
             footer={null}
             onCancel={handleCancel}
@@ -25,11 +40,23 @@ const ListOfAchievementsModal = (props: Props) => {
             <List
                 dataSource={props.achievementDoc}
                 renderItem={item => (
-                    <List.Item>
-                        <FileTextOutlined 
-                        className={classes.fileIcon}/>
+                    <List.Item
+                        actions={[
+                            <DownloadOutlined
+                                className={classes.downloadIcon}
+                                onClick={()=>downloadFile(item.blobName,item.fileName)}
+                            />,
+                            <DeleteOutlined
+                                className={classes.deleteIcon}
+                                onClick={() => deleteFIle(item.id, item.fileName)}
+                            />
+
+                        ]}>
+                        <FileTextOutlined
+                            className={classes.fileIcon} />
                         <List.Item.Meta
-                        title={item.fileName}
+                            className={classes.text}
+                            title={item.fileName}
                         />
                     </List.Item>
                 )}
