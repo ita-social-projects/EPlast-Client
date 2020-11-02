76499621565cd44c4063756a389220cebe021678
Taras Volosheniuk <vapvae@gmail.com>
2020-11-02 18:59:33 +0200

    Annual reports table reworkek with card component

---
+++ src/pages/AnnualReport/AnnualReportTable/CityAnnualReportTable.tsx
@@ -0,0 +1,255 @@
+import React, { useEffect, useState, PropsWithRef } from "react";
+import { useHistory } from "react-router-dom";
+import { Table, Spin, Input, Divider, Button } from 'antd';
+import AnnualReport from '../Interfaces/AnnualReport';
+import ClubAnnualReport from '../Interfaces/ClubAnnualReport';
+import Paragraph from 'antd/lib/skeleton/Paragraph';
+import ClickAwayListener from "react-click-away-listener";
+import UnconfirmedDropdown from "./Dropdowns/UnconfirmedDropdown/UnconfirmedDropdown";
+import ConfirmedDropdown from "./Dropdowns/ConfirmedDropdown/ConfirmedDropdown";
+import SavedDropdown from "./Dropdowns/SavedDropdown/SavedDropdown";
+import AnnualReportInformation from "./AnnualReportInformation/AnnualReportInformation";
+import CitySelectModal from "./CitySelectModal/CitySelectModal";
+import AnnualReportApi from "../../../api/AnnualReportApi";
+import Modal from "antd/lib/modal";
+import AuthStore from "../../../stores/AuthStore";
+import jwt_decode from "jwt-decode";
+
+
+
+interface props {
+
+    columns: any;
+    filteredData: any;
+  }
+
+  export const CityAnnualReportTable =({columns, filteredData}:props)=>{
+  const history = useHistory();
+    const [annualReport, setAnnualReport] = useState<AnnualReport>(Object);
+    const [clubAnnualReport, setClubAnnualReport] = useState<ClubAnnualReport>(Object);
+    const [reportStatusNames, setReportStatusNames] = useState<string[]>(Array());
+    const [annualReports, setAnnualReports] = useState<AnnualReport[]>(Array());
+    const [clubAnnualReports, setClubAnnualReports] = useState<ClubAnnualReport[]>(Array());
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+  const [recordObj, setRecordObj] = useState<any>(0);
+    const [loading, setLoading] = useState(false);
+    const [canManage, setCanManage] = useState<boolean>(false);
+    const [showUnconfirmedDropdown, setShowUnconfirmedDropdown] = useState<
+    boolean
+  >(false);
+  const [showCitySelectModal, setShowCitySelectModal] = useState<boolean>(
+    false
+  );
+  const [showConfirmedDropdown, setShowConfirmedDropdown] = useState<boolean>(
+    false
+  );
+  const [showAnnualReportModal, setShowAnnualReportModal] = useState<boolean>(
+    false
+  );
+  const [showSavedDropdown, setShowSavedDropdown] = useState<boolean>(false);
+
+    const hideDropdowns = () => {
+      setShowUnconfirmedDropdown(false);
+      setShowConfirmedDropdown(false);
+      setShowSavedDropdown(false);
+    };
+
+    const checkAccessToManage = () => {
+      let jwt = AuthStore.getToken() as string;
+      let decodedJwt = jwt_decode(jwt) as any;
+      let roles = decodedJwt[
+        "http://schemas.microsoft.com/ws/2008/06/identity/claims/role"
+      ] as string[];
+      setCanManage(roles.includes("Admin") || roles.includes("Голова Регіону"));
+    };
+    
+    const showDropdown = (annualReportStatus: number) => {
+      console.log(annualReportStatus)
+      switch (annualReportStatus) {
+        case 0:
+          hideDropdowns();
+          setShowUnconfirmedDropdown(true);
+          break;
+        case 1:
+          hideDropdowns();
+          setShowConfirmedDropdown(true);
+          break;
+        case 2:
+          hideDropdowns();
+          setShowSavedDropdown(true);
+          break;
+      }
+    };
+
+    const itemRender = (current: any, type: string, originalElement: any) => {
+      if (type === "prev") {
+        return <Button type="primary">Попередня</Button>;
+      }
+      if (type === "next") {
+        return <Button type="primary">Наступна</Button>;
+      }
+      return originalElement;
+    };
+    const handleView = async (id: number) => {
+      console.log(id)
+      hideDropdowns();
+      try {
+        let response = await AnnualReportApi.getById(id);
+        console.log(id)
+        setAnnualReport(response.data.annualReport);
+        console.log(annualReport)
+        setShowAnnualReportModal(true);
+      } catch (error) {
+        showError(error.message);
+      }
+    };
+    const showError = (message: string) => {
+      Modal.error({
+        title: "Помилка!",
+        content: message,
+      });
+    };
+    const showSuccess = (message: string) => {
+      Modal.success({
+        content: message,
+      });
+    };
+    const handleEdit = (id: number) => {
+      hideDropdowns();
+      history.push(`/annualreport/edit/${id}`);
+    };
+    const handleConfirm = async (id: number) => {
+      hideDropdowns();
+      try {
+        let response = await AnnualReportApi.confirm(id);
+        let cityId = annualReports.find((item) => item.id == id)?.cityId;
+        setAnnualReports(
+          annualReports.map((item) => {
+            if (
+              item.id === id ||
+              (item.id !== id && item.cityId === cityId && item.status === 1)
+            ) {
+              item.status++;
+            }
+            return item;
+          })
+        );
+        showSuccess(response.data.message);
+      } catch (error) {
+        showError(error.message);
+      }
+    };
+    const handleRemove = async (id: number) => {
+      hideDropdowns();
+      try {
+        let response = await AnnualReportApi.remove(id);
+        setAnnualReports(annualReports?.filter((item) => item.id !== id));
+        showSuccess(response.data.message);
+      } catch (error) {
+        showError(error.message);
+      }
+    };
+    const handleCancel = async (id: number) => {
+      hideDropdowns();
+      try {
+        let response = await AnnualReportApi.cancel(id);
+        setAnnualReports(
+          annualReports.map((item) => {
+            if (item.id === id) {
+              item.status--;
+            }
+            return item;
+          })
+        );
+        showSuccess(response.data.message);
+      } catch (error) {
+        showError(error.message);
+      }
+    };
+    useEffect(() => {
+
+      checkAccessToManage();
+    }, []);
+    return (
+        
+        <div>
+              <Table
+        bordered
+        rowKey="id"
+        columns={columns}
+        dataSource={filteredData}
+        onRow={(record) => {
+          return {
+            onClick: () => {
+              hideDropdowns();
+            },
+            onContextMenu: (event) => {
+              console.log(record)
+              event.preventDefault();
+              showDropdown(record.status);
+              setAnnualReport(record);
+              setX(event.pageX);
+              setY(event.pageY-200);
+            },
+          };
+        }}
+        onChange={(pagination) => {
+          if (pagination) {
+            window.scrollTo({
+              left: 0,
+              top: 0,
+              behavior: "smooth",
+            });
+          }
+        }}
+        pagination={{
+          itemRender,
+          position: ["bottomRight"],
+          showTotal: (total, range) =>
+            `Записи з ${range[0]} по ${range[1]} із ${total} записів`,
+        }}
+      />
+      <ClickAwayListener onClickAway={hideDropdowns}>
+        <UnconfirmedDropdown
+          showDropdown={showUnconfirmedDropdown}
+          record={annualReport}
+          pageX={x}
+          pageY={y}
+          canManage={canManage}
+          onView={handleView}
+          onEdit={handleEdit}
+          onConfirm={handleConfirm}
+          onRemove={handleRemove}
+        />
+        <ConfirmedDropdown
+          showDropdown={showConfirmedDropdown}
+          record={annualReport}
+          pageX={x}
+          pageY={y}
+          canManage={canManage}
+          onView={handleView}
+          onCancel={handleCancel}
+        />
+        <SavedDropdown
+          showDropdown={showSavedDropdown}
+          record={annualReport}
+          pageX={x}
+          pageY={y}
+          onView={handleView}
+        />
+      </ClickAwayListener>
+      <AnnualReportInformation
+        visibleModal={showAnnualReportModal}
+        annualReport={annualReport}
+        showError={showError}
+        handleOk={() => setShowAnnualReportModal(false)}
+      />
+      <CitySelectModal
+        visibleModal={showCitySelectModal}
+        handleOk={() => setShowCitySelectModal(false)}
+      />
+        </div>
+        
+        )
+}
