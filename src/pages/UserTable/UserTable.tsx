dac1e164abdb4d675e431c059f059c280c0c0f24
vovikboyko <31668723+vovikboyko@users.noreply.github.com>
2021-01-20 13:04:34 +0200

    Added tabs to userTable

--- src/pages/UserTable/UserTable.tsx
+++ src/pages/UserTable/UserTable.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Table, Input, Layout, Row, Col, Button } from "antd";
+import { Table, Input, Layout, Card } from "antd";
 import adminApi from "../../api/adminApi";
 import DropDownUserTable from "./DropDownUserTable";
 import Title from "antd/lib/typography/Title";
@@ -7,18 +7,19 @@ import ColumnsForUserTable from "./ColumnsForUserTable";
 import UserTable from "../../models/UserTable/UserTable";
 import Spinner from "../Spinner/Spinner";
 import ClickAwayListener from "react-click-away-listener";
-import moment from "moment";
-const classes = require("./UserTable.module.css");
+import classes from "./UserTable.module.css";
 
 const UsersTable = () => {
-  const [recordObj, setRecordObj] = useState<any>(0);
-  const [showDropdown, setShowDropdown] = useState(false);
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const [updatedUser, setUpdatedUser] = useState<UserTable[]>([]);
+  const [users, setUsers] = useState<UserTable[]>([]);
   const [loading, setLoading] = useState(false);
   const [searchedData, setSearchedData] = useState("");
-  const [users, setUsers] = useState<UserTable[]>([]);
-  const [updatedUser, setUpdatedUser] = useState<UserTable[]>([]);
+  const [viewedUsers, setViewedUsers] = useState<UserTable[]>([]);
+  const [noTitleKey, setKey] = useState<string>("confirmed");
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [recordObj, setRecordObj] = useState<any>(0);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
   const [roles, setRoles] = useState<string>();
 
   useEffect(() => {
@@ -34,20 +35,11 @@ const UsersTable = () => {
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchedData(event.target.value.toLowerCase());
-  };
-
-  const itemRender = (current: any, type: string, originalElement: any) => {
-    if (type === "prev") {
-      return <Button type="primary">Попередня</Button>;
-    }
-    if (type === "next") {
-      return <Button type="primary">Наступна</Button>;
-    }
-    return originalElement;
+    onTabChange(noTitleKey);
   };
 
   let filteredData = searchedData
-    ? users.filter((item) => {
+    ? viewedUsers.filter((item) => {
         return Object.values([
           item.regionName,
           item.cityName,
@@ -59,10 +51,10 @@ const UsersTable = () => {
           return String(element).toLowerCase().includes(searchedData);
         });
       })
-    : users;
+    : viewedUsers;
 
   filteredData = filteredData.concat(
-    users.filter(
+    viewedUsers.filter(
       (item) =>
         (item.user.firstName?.toLowerCase()?.includes(searchedData) ||
           item.user.lastName?.toLowerCase()?.includes(searchedData) ||
@@ -74,8 +66,8 @@ const UsersTable = () => {
 
   const handleDelete = (id: string) => {
     fetchData();
-    const filteredData = users.filter((d: any) => d.id !== id);
-    setUsers([...filteredData]);
+    const usersAfterDeleting = users.filter((d: any) => d.id !== id);
+    setUsers([...usersAfterDeleting]);
   };
 
   const handleClickAway = () => {
@@ -83,15 +75,53 @@ const UsersTable = () => {
   };
 
   const handleChange = (id: string, userRole: string) => {
-    const filteredData = users.filter((d: any) => {
+    const usersAfterRoleChanging = users.filter((d: any) => {
       if (d.id === id) {
         d.userRoles += ", " + userRole;
       }
       return d;
     });
-    setUpdatedUser([...filteredData]);
-    setUsers([...filteredData]);
+    setUpdatedUser([...usersAfterRoleChanging]);
+    setUsers([...usersAfterRoleChanging]);
   };
+
+  const tabList = [
+    {
+      key: "confirmed",
+      tab: "Всі користувачі",
+    },
+    {
+      key: "interested",
+      tab: "Зацікавлені",
+    },
+    {
+      key: "unconfirmed",
+      tab: "Непідтверджені",
+    },
+  ];
+
+  const onTabChange = (key: string) => {
+    setKey(key);
+    key == "confirmed"
+      ? (filteredData = users.filter((u) => u.user.emailConfirmed == true))
+      : key == "interested"
+      ? (filteredData = users.filter((u) =>
+          u.userRoles.includes("Зацікавлений")
+        ))
+      : (filteredData = users.filter((u) => u.user.emailConfirmed == false));
+
+    setViewedUsers(
+      key == "confirmed"
+        ? users.filter((u) => u.user.emailConfirmed == true)
+        : key == "interested"
+        ? users.filter((u) => u.userRoles.includes("Зацікавлений"))
+        : users.filter((u) => u.user.emailConfirmed == false)
+    );
+  };
+
+  useEffect(() => {
+    onTabChange("confirmed");
+  }, [users]);
 
   return loading === false ? (
     <Spinner />
@@ -105,43 +135,52 @@ const UsersTable = () => {
       <div className={classes.searchContainer}>
         <Input placeholder="Пошук" onChange={handleSearch} allowClear />
       </div>
-      <Table
-        className={classes.table}
-        bordered
-        rowKey="id"
-        scroll={{ x: 1450 }}
-        columns={ColumnsForUserTable}
-        dataSource={filteredData}
-        onRow={(record) => {
-          return {
-            onClick: () => {
-              setShowDropdown(false);
-            },
-            onContextMenu: (event) => {
-              event.preventDefault();
-              setShowDropdown(true);
-              setRecordObj(record.user.id);
-              setRoles(record.userRoles);
-              setX(event.pageX);
-              setY(event.pageY);
-            },
-          };
+      <Card
+        style={{ width: "100%" }}
+        tabList={tabList}
+        activeTabKey={noTitleKey}
+        onTabChange={(key) => {
+          onTabChange(key);
         }}
-        onChange={(pagination) => {
-          if (pagination) {
-            window.scrollTo({
-              left: 0,
-              top: 0,
-              behavior: "smooth",
-            });
-          }
-        }}
-        pagination={{
-          showLessItems: true,
-          responsive: true,
-          showSizeChanger: true,
-        }}
-      />
+      >
+        <Table
+          className={classes.table}
+          bordered
+          rowKey="id"
+          scroll={{ x: 1450 }}
+          columns={ColumnsForUserTable}
+          dataSource={filteredData}
+          onRow={(record) => {
+            return {
+              onClick: () => {
+                setShowDropdown(false);
+              },
+              onContextMenu: (event) => {
+                event.preventDefault();
+                setShowDropdown(true);
+                setRecordObj(record.user.id);
+                setRoles(record.userRoles);
+                setX(event.pageX);
+                setY(event.pageY);
+              },
+            };
+          }}
+          onChange={(pagination) => {
+            if (pagination) {
+              window.scrollTo({
+                left: 0,
+                top: 0,
+                behavior: "smooth",
+              });
+            }
+          }}
+          pagination={{
+            showLessItems: true,
+            responsive: true,
+            showSizeChanger: true,
+          }}
+        />
+      </Card>
       <ClickAwayListener onClickAway={handleClickAway}>
         <DropDownUserTable
           showDropdown={showDropdown}
