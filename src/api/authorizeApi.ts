12bdc941e674491d3367d22ec99613caf8ee86ba
vovikboyko <31668723+vovikboyko@users.noreply.github.com>
2021-01-20 16:32:40 +0200

    Changed controller of few methods

--- src/api/authorizeApi.ts
+++ src/api/authorizeApi.ts
@@ -1,39 +1,37 @@
 import Api from "./api";
-import notificationLogic from '../components/Notifications/Notification';
-import AuthStore from '../stores/AuthStore';
+import notificationLogic from "../components/Notifications/Notification";
+import AuthStore from "../stores/AuthStore";
 import { string } from "yup";
-import FacebookData from '../pages/SignIn/FacebookDataInterface';
+import FacebookData from "../pages/SignIn/FacebookDataInterface";
 
 export default class AuthorizeApi {
-
   static isSignedIn(): boolean {
     return !!AuthStore.getToken();
   }
 
   login = async (data: any) => {
     const response = await Api.post("Auth/signin", data)
-      .then(response => {
+      .then((response) => {
         if (response.data.token !== null) {
           AuthStore.setToken(response.data.token);
         }
       })
-      .catch(error => {
+      .catch((error) => {
         if (error.response.status === 400) {
-          notificationLogic('error', error.response.data.value);
+          notificationLogic("error", error.response.data.value);
         }
-      })
+      });
     return response;
   };
 
-
   register = async (data: any) => {
     const response = await Api.post("Auth/signup", data)
-      .then(response => {
-        notificationLogic('success', response.data.value);
+      .then((response) => {
+        notificationLogic("success", response.data.value);
       })
-      .catch(error => {
+      .catch((error) => {
         if (error.response.status === 400) {
-          notificationLogic('error', error.response.data.value);
+          notificationLogic("error", error.response.data.value);
         }
       });
     return response;
@@ -41,12 +39,12 @@ export default class AuthorizeApi {
 
   forgotPassword = async (data: any) => {
     const response = await Api.post("Auth/forgotPassword", data)
-      .then(response => {
-        notificationLogic('success', response.data.value);
+      .then((response) => {
+        notificationLogic("success", response.data.value);
       })
-      .catch(error => {
+      .catch((error) => {
         if (error.response.status === 400) {
-          notificationLogic('error', error.response.data.value);
+          notificationLogic("error", error.response.data.value);
         }
       });
     return response;
@@ -54,25 +52,25 @@ export default class AuthorizeApi {
 
   resetPassword = async (data: any) => {
     const response = await Api.post("Auth/resetPassword", data)
-      .then(response => {
-        notificationLogic('success', response.data.value);
+      .then((response) => {
+        notificationLogic("success", response.data.value);
       })
-      .catch(error => {
+      .catch((error) => {
         if (error.response.status === 400) {
-          notificationLogic('error', error.response.data.value);
+          notificationLogic("error", error.response.data.value);
         }
       });
     return response;
   };
 
   changePassword = async (data: any) => {
-    const response = await Api.post("Auth/changePassword", data)
-      .then(response => {
-        notificationLogic('success', response.data.value);
+    const response = await Api.post("Problem/changePassword", data)
+      .then((response) => {
+        notificationLogic("success", response.data.value);
       })
-      .catch(error => {
+      .catch((error) => {
         if (error.response.status === 400) {
-          notificationLogic('error', error.response.data.value);
+          notificationLogic("error", error.response.data.value);
         }
       });
     return response;
@@ -83,54 +81,56 @@ export default class AuthorizeApi {
   };
 
   sendQuestionAdmin = async (data: any) => {
-    const response = await Api.post("Auth/sendQuestion", data)
-      .then(response => {
-        notificationLogic('success', response.data.value);
+    const response = await Api.post("Problem/sendQuestion", data)
+      .then((response) => {
+        notificationLogic("success", response.data.value);
       })
-      .catch(error => {
+      .catch((error) => {
         if (error.response.status === 400) {
-          notificationLogic('error', error.response.data.value);
+          notificationLogic("error", error.response.data.value);
         }
       });
     return response;
   };
 
-  sendToken=async (token: string) => {
+  sendToken = async (token: string) => {
     const response = await Api.post(`Auth/signin/google/?googleToken=${token}`)
-      .then(response => {
+      .then((response) => {
         if (response.data.token !== null) {
           AuthStore.setToken(response.data.token);
         }
       })
-      .catch(error => {
+      .catch((error) => {
         if (error.response.status === 400) {
-          notificationLogic('error', error.response.data.value);
+          notificationLogic("error", error.response.data.value);
         }
-      })
+      });
     return response;
   };
-  
+
   getGoogleId = async () => {
-    
     const response = await Api.get("Auth/GoogleClientId");
 
     return response.data;
   };
 
-  sendFacebookInfo = async(response: FacebookData)=> {
-    const respon = await Api.post(`Auth/signin/facebook`,JSON.stringify(response))
-      .then(respon => {
+  sendFacebookInfo = async (response: FacebookData) => {
+    const respon = await Api.post(
+      `Auth/signin/facebook`,
+      JSON.stringify(response)
+    )
+      .then((respon) => {
         if (respon.data.token !== null) {
           AuthStore.setToken(respon.data.token);
         }
       })
-      .catch(error => {
+      .catch((error) => {
         if (error.response.status === 400) {
-          notificationLogic('error', error.response.data.value);
+          notificationLogic("error", error.response.data.value);
         }
-      })
+      });
     return respon;
-  }
+  };
 
   getFacebookId = async () => {
     const response = await Api.get("Auth/FacebookAppId");
