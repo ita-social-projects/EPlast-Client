635d9ef9de9b7ec0ae5d0a54cb4c29721a5e31d3
Andrii52 <andrii723@gmail.com>
2021-06-30 12:46:41 +0300

    Changed image extension to allow uppercase.

--- src/pages/userPage/EditUserPage/EditUserPage.tsx
+++ src/pages/userPage/EditUserPage/EditUserPage.tsx
@@ -252,7 +252,7 @@ export default function () {
   };
   
   const checkFile = (size: number, fileName: string) => {
-    const extension = fileName.split(".").reverse()[0];
+    const extension = fileName.split(".").reverse()[0].toLowerCase();
     const isCorrectExtension =
       extension.indexOf("jpeg") !== -1 ||
       extension.indexOf("jpg") !== -1 ||
