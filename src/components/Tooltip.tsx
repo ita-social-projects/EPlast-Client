49bfcba40f65c1ae0f9759703acaccf9d50d3133
christykobyletska <kobyletskachristy@gmail.com>
2021-08-17 16:59:32 +0300

    small addition

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,12 +1,13 @@
 import { Tooltip } from "antd";
 import React from "react";
 
+export const parameterMaxLength = 21;
 function addTooltip (number: number, text: string) {
    return (text?.length > number) ?
         <Tooltip title={text}>
             <span>{text.slice(0, number - 1) + "..."}</span>
         </Tooltip>
     : text  
-  }
+}
   
 export default addTooltip;
