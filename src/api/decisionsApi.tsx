bedd20052af72b53f12b5738cd80bedadce213e1
Ніколь Козманко <nikakozmank@gmail.com>
2021-08-18 16:31:32 +0300

    Fixed Comfirmed field

--- src/api/decisionsApi.tsx
+++ src/api/decisionsApi.tsx
@@ -1,21 +1,21 @@
 import Api from "./api";
 import notificationLogic from '../components/Notifications/Notification';
-import{successfulCreateAction, successfulEditAction, successfulDeleteAction} from "../components/Notifications/Messages"
+import { successfulCreateAction, successfulEditAction, successfulDeleteAction } from "../components/Notifications/Messages"
 
 export type Decision = {
   id: number;
   name: string;
   governingBody: string;
   decisionStatusType: string;
-  decisionTarget : string;
+  decisionTarget: string;
   description: string;
-  date : Date;
+  date: Date;
   userId: string;
   fileName: string | null;
 }
 export type FileWrapper = {
   FileAsBase64: string | null;
-  FileName : string | null;
+  FileName: string | null;
 }
 export type GoverningBody = {
   id: number;
@@ -25,26 +25,24 @@ export type GoverningBody = {
   phoneNumber: string;
   email: string;
 }
-
-export type decisionTarget ={
-  id : number;
+export type decisionTarget = {
+  id: number;
   targetName: string;
 }
-
-export type decisionStatusType ={
+export type decisionStatusType = {
   text: string;
   value: string;
 }
 export type DecisionOnCreateData = {
   governingBodies: GoverningBody[];
-  decisionStatusTypeListItems :decisionStatusType[];
-  decisionTargets : decisionTarget[];
+  decisionStatusTypeListItems: decisionStatusType[];
+  decisionTargets: decisionTarget[];
 }
 export type DecisionWrapper = {
   decision: DecisionPost;
   fileAsBase64: string | null;
 }
-export type DecisionPost  ={
+export type DecisionPost = {
   id: number;
   name: string;
   decisionStatusType: number;
@@ -55,86 +53,85 @@ export type DecisionPost  ={
   userId: string;
   fileName: string | null;
 };
- export const statusTypePostParser = (statusType: decisionStatusType): number =>{
-  if(statusType.value === "InReview") return 0;
+export const statusTypePostParser = (statusType: decisionStatusType): number => {
+  if (statusType.value === "InReview") return 0;
   if (statusType.value === "Confirmed") return 1;
   return 2;
-  };
-  export const statusTypeGetParser = (statusType: number): string =>{
-    if(statusType === 0) return "У розгляді";
-    if (statusType === 1) return "Підтверджено";
-    if (statusType === 2) return "Скасовано";
-    return "Не визначено";
-    };
-const dataURLtoFile = (dataurl : string, filename: string)=>{
+};
+export const statusTypeGetParser = (statusType: number): string => {
+  if (statusType === 0) return "У розгляді";
+  if (statusType === 1) return "Потверджено";
+  if (statusType === 2) return "Скасовано";
+  return "Не визначено";
+};
+const dataURLtoFile = (dataurl: string, filename: string) => {
   const arr = dataurl.split(',');
   const mime = arr[0].match(/:(.*?);/)![1];
   const bstr = atob(arr[1]);
-  let {length} = bstr;
+  let { length } = bstr;
   const u8arr = new Uint8Array(length);
 
-while(length !== -1){
-  u8arr[length] = bstr.charCodeAt(length);
-  length-=1;
-}
+  while (length !== -1) {
+    u8arr[length] = bstr.charCodeAt(length);
+    length -= 1;
+  }
 
-return new File([u8arr], filename, {type:mime});
+  return new File([u8arr], filename, { type: mime });
 }
-  const getById =  async (id: number)=> {
-    const response : DecisionPost = await (await Api.get(`Decisions/${id}`)).data;
-    return  response;
-  };
-    
-  const getAll =  async() =>{
-    const {data} = await Api.get("Decisions");
-    
-    return data;
-  };
-  
-  const getAllDecisionsForTable = async (searchedData: string, page: number, pageSize: number) => {
-    return (await Api.get('Decisions/DecisionsForTable',
-        {
-            searchedData: searchedData,
-            page: page,
-            pageSize: pageSize,
-        })).data;
-  };
+const getById = async (id: number) => {
+  const response: DecisionPost = await (await Api.get(`Decisions/${id}`)).data;
+  return response;
+};
 
-  const getOnCreate = async () => {
-    const data : DecisionOnCreateData = await (await Api.get(`Decisions/NewDecision`)).data;
-   
-    return  data;
+const getAll = async () => {
+  const { data } = await Api.get("Decisions");
+
+  return data;
+};
+
+const getAllDecisionsForTable = async (searchedData: string, page: number, pageSize: number) => {
+  return (await Api.get('Decisions/DecisionsForTable',
+    {
+      searchedData: searchedData,
+      page: page,
+      pageSize: pageSize,
+    })).data;
+};
+
+const getOnCreate = async () => {
+  const data: DecisionOnCreateData = await (await Api.get(`Decisions/NewDecision`)).data;
+  return data;
+};
+const getPdf = async (id: number) => {
+  const data = await (await Api.get(`Decisions/createpdf/${id}`)).data;
+  const binaryString = window.atob(data);
+  const binaryLen = binaryString.length;
+  const bytes = new Uint8Array(binaryLen);
+  for (let i = 0; i < binaryLen; i += 1) {
+    const ascii = binaryString.charCodeAt(i);
+    bytes[i] = ascii;
   };
-  const getPdf = async (id: number) => {
-    const data = await (await Api.get(`Decisions/createpdf/${id}`)).data;
-    const binaryString = window.atob(data);
-    const binaryLen = binaryString.length;
-    const bytes = new Uint8Array(binaryLen);
-    for (let i = 0; i < binaryLen; i += 1) {
-      const ascii = binaryString.charCodeAt(i);
-      bytes[i] = ascii;
-      };
-    const blob = new Blob([bytes], {type: "application/pdf"});
-    const link = window.URL.createObjectURL(blob);
-    return  link;
-  };
-  const post = async (data : any) => {
-    const response = await Api.post("Decisions",data).then(response =>{
-      notificationLogic('success', successfulCreateAction("Рішення"));
-    })
+  const blob = new Blob([bytes], { type: "application/pdf" });
+  const link = window.URL.createObjectURL(blob);
+  return link;
+};
+const post = async (data: any) => {
+  const response = await Api.post("Decisions", data).then(response => {
+    notificationLogic('success', successfulCreateAction("Рішення"));
+  })
     .catch(error => {
-        notificationLogic('error', error.response.data.value);
+      notificationLogic('error', error.response.data.value);
     });
-    return response;
-  };
-  const postForCheckFile = async (data : any) => {
-    const response = await Api.post("Decisions/CheckFile", data)
+  return response;
+};
+const postForCheckFile = async (data: any) => {
+  const response = await Api.post("Decisions/CheckFile", data)
     .catch(error => {
-        notificationLogic('error', error.response.data.value);
+      notificationLogic('error', error.response.data.value);
     });
-    return response;
-  };
-const getFileAsBase64 = async (fileName: string) =>{
+  return response;
+};
+const getFileAsBase64 = async (fileName: string) => {
   const response = await (await Api.get(`Decisions/downloadfile/${fileName}`)).data;
   const file = dataURLtoFile(response, fileName);
   const anchor = window.document.createElement('a');
@@ -146,25 +143,25 @@ const getFileAsBase64 = async (fileName: string) =>{
   window.URL.revokeObjectURL(anchor.href);
   return response;
 }
-  const put = async (id: number, data : DecisionPost) =>{
-    const response = await Api.put(`Decisions/${id}`,data).then(response =>{
-      notificationLogic('success', successfulEditAction("Рішення"));
-    })
+const put = async (id: number, data: DecisionPost) => {
+  const response = await Api.put(`Decisions/${id}`, data).then(response => {
+    notificationLogic('success', successfulEditAction("Рішення"));
+  })
     .catch(error => {
-        notificationLogic('error', error.response.data.value);
+      notificationLogic('error', error.response.data.value);
     });
-    
-    return response;
-  };
-  
-  const remove = async (id : number) => {
-    const response = await Api.remove(`Decisions/${id}`).then(response =>{
-      notificationLogic('success',  successfulDeleteAction("Рішення"));
-    })
+
+  return response;
+};
+
+const remove = async (id: number) => {
+  const response = await Api.remove(`Decisions/${id}`).then(response => {
+    notificationLogic('success', successfulDeleteAction("Рішення"));
+  })
     .catch(error => {
-        notificationLogic('error', error.response.data.value);
+      notificationLogic('error', error.response.data.value);
     });
-    return response;
-  };
+  return response;
+};
 
 export default { getById, getAll, getAllDecisionsForTable, getOnCreate, getPdf, getFileAsBase64, post, postForCheckFile, put, remove };
--- src/pages/DecisionTable/FormAddDecision.tsx
+++ src/pages/DecisionTable/FormAddDecision.tsx
@@ -398,11 +398,9 @@ const FormAddDecision: React.FC<FormAddDecisionProps> = (props: any) => {
               className={formclasses.selectField}
               getPopupContainer={(triggerNode) => triggerNode.parentNode}
             >
-              {data?.decisionStatusTypeListItems.map((dst) => (
-                <Select.Option key={dst.value} value={JSON.stringify(dst)}>
-                  {dst.text}
-                </Select.Option>
-              ))}
+              <Select.Option key="9" value="InReview">Прийнято</Select.Option>
+              <Select.Option key="10" value="Confirmed">Потверджено</Select.Option>
+              <Select.Option key="11" value="Canceled">Скасовано</Select.Option>
             </Select>
           </Form.Item>
         </Col>
