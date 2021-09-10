55f3dbc1b4a96f3e1461d283a33f34fb2415b856
Julia <iphotet@gmail.com>
2021-09-10 23:49:56 +0300

    edited_button

--- src/pages/Actions/ActionEvent/EventCalendar/EventCalendar.tsx
+++ src/pages/Actions/ActionEvent/EventCalendar/EventCalendar.tsx
@@ -81,6 +81,10 @@ export default function () {
                     <FullCalendar
                         plugins={[dayGridPlugin]}
                         initialView="dayGridMonth"
+                        buttonText= {
+                           
+                            {today: 'Поточний місяць'}
+                           }
                         headerToolbar={{
                             left: 'prev,next today',
                             center: 'title',
