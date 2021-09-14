25203abd9bf1218c8007bbc15504a6a3f1ccd1d7
christykobyletska <kobyletskachristy@gmail.com>
2021-09-14 12:28:48 +0300

    Added a few impovements (events, city,club)

--- src/pages/Club/Club/Club.tsx
+++ src/pages/Club/Club/Club.tsx
@@ -105,11 +105,11 @@ const Club = () => {
     const follower = await addFollower(+id);
     if (club.head !== null ){
       await createNotification(club.head.userId,
-        `Приєднався новий прихильник: ${follower.data.user.firstName} ${follower.data.user.lastName} до вашого куреня`, true);   
+        `Новий прихильник приєднався: ${follower.data.user.firstName} ${follower.data.user.lastName} до вашого куреня`, true);   
     }
     if (club.headDeputy !== null ){
       await createNotification(club.headDeputy.userId,
-        `Приєднався новий прихильник: ${follower.data.user.firstName} ${follower.data.user.lastName} до вашого куреня`, true);   
+        `Новий прихильник приєднався: ${follower.data.user.firstName} ${follower.data.user.lastName} до вашого куреня`, true);   
     }
     follower.data.user.imagePath = (
       await userApi.getImage(follower.data.user.imagePath)
