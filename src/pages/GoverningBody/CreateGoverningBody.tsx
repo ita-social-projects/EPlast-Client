4bbda69ee9c1aebc4dc283ab3de669a06d94e5f9
Dmytro <84271714+DmytroZelinsky@users.noreply.github.com>
2021-09-16 21:07:11 +0300

    Update CreateGoverningBody.tsx

--- src/pages/GoverningBody/CreateGoverningBody.tsx
+++ src/pages/GoverningBody/CreateGoverningBody.tsx
@@ -115,7 +115,6 @@ const CreateGoverningBody = () => {
         response.logo = logo.data;
       }
       setGoverningBody(response);
-      console.log(response)
     } finally {
       setLoading(false);
     }
