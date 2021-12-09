714898e83ac6901c5c78084ab847167ef9bd75e9
YuriiDzioba <dzioba99yurii@gmail.com>
2021-12-09 16:29:36 +0200

    Terms

--- src/components/PrivateLayout/PrivateLayout.tsx
+++ src/components/PrivateLayout/PrivateLayout.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useHistory, Link } from "react-router-dom";
 import { Avatar, Layout, Menu, Button } from "antd";
-import { BankOutlined, BookOutlined, RollbackOutlined } from "@ant-design/icons";
+import { BankOutlined, BookOutlined, RollbackOutlined, AlignLeftOutlined } from "@ant-design/icons";
 import {
   SolutionOutlined,
   SnippetsOutlined,
@@ -259,6 +259,17 @@ const PrivateLayout = ({ children }: any) => {
                 Про Базу
               </Menu.Item>
             ) : (<> </>)
+            }
+            {(canEdit === true || canSee === true || canAccess === true || regionAdm === true || cityAdm === true || clubAdm === true) ? (
+                <Menu.Item 
+                  onClick={() => { handleClickAway(); history.push('/terms'); }} 
+                  key="18"
+                  title=""
+                >
+                  <AlignLeftOutlined />
+                    Політика конфіденційності
+                </Menu.Item>
+                ) : (<> </>)
             }
           </Menu>
         </Sider>
