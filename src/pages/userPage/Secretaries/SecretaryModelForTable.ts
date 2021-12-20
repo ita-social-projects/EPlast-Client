a44287a836421bd64af12def8aa7187e7747f3cc
IlliaDR <omen666i@gmail.com>
2021-12-20 13:47:41 +0200

    Updated GoverningBodiesTable

---
+++ src/pages/userPage/Secretaries/SecretaryModelForTable.ts
@@ -0,0 +1,12 @@
+type SecretaryModelForTable = {
+        id: number;
+        userId: string;
+        userName: string;
+        adminType: string;
+        startDate: Date;
+        endDate: Date;
+        governBodyName: string;
+        status: boolean;
+}
+
+export default SecretaryModelForTable;
