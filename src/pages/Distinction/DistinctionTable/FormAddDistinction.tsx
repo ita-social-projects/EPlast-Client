67e3bdccad0f52aea1ff1013fedaa741821be484
DimaDiD <58574074+DimaDiD@users.noreply.github.com>
2022-03-16 14:10:29 +0200

    Update FormAddDistinction.tsx
    edit form id

--- src/pages/Distinction/DistinctionTable/FormAddDistinction.tsx
+++ src/pages/Distinction/DistinctionTable/FormAddDistinction.tsx
@@ -147,7 +147,7 @@ const FormAddDistinction: React.FC<FormAddDistinctionProps> = (props: any) => {
       name="basic"
       onFinish={handleSubmit}
       form={form}
-      id="area"
+      id="addArea"
       style={{ position: "relative" }}
     >
       <Row justify="start" gutter={[12, 0]}>
@@ -308,7 +308,7 @@ const FormAddDistinction: React.FC<FormAddDistinctionProps> = (props: any) => {
               format={dateFormat}
               className={formclasses.selectField}
               getPopupContainer={() =>
-                document.getElementById("area")! as HTMLElement
+                document.getElementById("addArea")! as HTMLElement
               }
               popupStyle={{ position: "absolute" }}
             />
