9480a684d958eb338acc8c39088bf825b335f7be
Bozhena Kondrat <89923581+bkondrat@users.noreply.github.com>
2022-05-10 12:24:26 +0200

    Update src/pages/Actions/ActionEvent/EventCalendar/EventCalendar.tsx

--- src/pages/Actions/ActionEvent/EventCalendar/EventCalendar.tsx
+++ src/pages/Actions/ActionEvent/EventCalendar/EventCalendar.tsx
@@ -131,7 +131,7 @@ export default function () {
               >
                 Закрити
               </Button>
-            </div>,
+            </div>
           ]}
         >
           <div className={classes.title}>
