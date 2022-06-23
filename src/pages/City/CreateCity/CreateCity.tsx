b95c2178ed24de5bb3620fcc9ce2922bb253ce2b
v4k <55637550+vsynieok@users.noreply.github.com>
2022-06-23 23:09:16 +0300

    Replace the pattern for email

--- src/pages/City/CreateCity/CreateCity.tsx
+++ src/pages/City/CreateCity/CreateCity.tsx
@@ -563,9 +563,6 @@ const CreateCity = () => {
                 name="email"
                 label="Електронна пошта"
                 labelCol={{ span: 24 }}
-                initialValue={
-                  isFollowerPath ? regionFollower.email : city.email
-                }
                 rules={descriptionValidation.Email}
               >
                 <Input
