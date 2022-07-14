6b8549ca4672ab68f1307e7e3a89b3829b0d453b
Goven112 <60715039+Goven112@users.noreply.github.com>
2022-07-14 16:19:00 +0300

    add-link-on-list-certificates

--- src/pages/userPage/personalData/Course.tsx
+++ src/pages/userPage/personalData/Course.tsx
@@ -3,7 +3,7 @@ import { Alert, Button, Col, Form, Input, Skeleton, Tooltip } from "antd";
 import moment from "moment";
 import { useParams, useHistory, Link } from "react-router-dom";
 import { StickyContainer } from "react-sticky";
-
+import jwt from "jwt-decode";
 
 import PlastLogo from "../../../assets/images/logo_PLAST.png"
 import AvatarAndProgressStatic from "./AvatarAndProgressStatic";
@@ -15,15 +15,27 @@ import { getAllCourse, getAllCourseByUserId } from "../../../api/courseApi";
 import Course from "../../../models/Course/Course";
 import Title from "antd/lib/typography/Title";
 import { debug } from "console";
+import ListOfAchievementsModal from "../Blanks/UserAchievements/ListOfAchievementsModal";
+import BlankDocument from "../../../models/Blank/BlankDocument";
+import { getAllAchievementDocumentsByUserId } from "../../../api/blankApi";
+import AuthLocalStorage from "../../../AuthLocalStorage";
 
 export default function () {
 
   const [visibleAchievementModal, setvisibleAchievementModal] = useState(false);
-
+  const [visibleListModal, setvisibleListModal] = useState(false);
   const [showAchievementModal, setshowAchievementModal] = useState(false);
   const [isDataLoaded, setDataLoaded] = useState<boolean>(false);
-
+  
   const [allCourses, setallCourses] = useState<Course[]>([]);
+  const [achievementDoc, setAchievementDoc] = useState<BlankDocument[]>([]);
+  const [userToken, setUserToken] = useState<any>([
+    {
+      nameid: "",
+    },
+  ]);
+
+
 
   const { userId } = useParams<{ userId: string }>();
   const history = useHistory();
@@ -31,28 +43,33 @@ export default function () {
     userProfile,
     activeUserRoles,
     activeUserId,
+    userProfileAccess,
     activeUserProfile,
     loading,
   } = useContext(PersonalDataContext);
 
 
   const urlaim = `/userpage/blank/${activeUserId}`;
-  const getButton = async () => {
+
+  const showUploadWindow = async () => {
     setvisibleAchievementModal(true);
-   
+  };
+  const showListCertificates = async () => {
+    setvisibleListModal(true);
   };
 
   const fetchData = async () => {
-    debugger
+    const token = AuthLocalStorage.getToken() as string;
+    setUserToken(jwt(token));
     const response = await getAllCourseByUserId(activeUserId);
+    const response1 = await getAllAchievementDocumentsByUserId(activeUserId);
+    setAchievementDoc(response1.data);
     setallCourses(response.data);
     setDataLoaded(true);
   };
 
   useEffect(() => {
     fetchData();
-    
-   
   }, []);
 
 
@@ -74,10 +91,8 @@ export default function () {
         
           <Col>
           <Title level={2}>{sectitem.name}</Title>
-          
-          
+        
             <p> <strong>{userProfile?.user.firstName} </strong>, пройдіть курс для продовження співпраці з нами   </p>
-
 
             <div className="rowBlock">
            
@@ -111,28 +126,45 @@ export default function () {
                     type="primary"
                     
                     className="buttonaddcertificate"
-                    onClick={() => getButton()
+                    onClick={() => showUploadWindow()
                     
                     }
                   >
                     Додати сертифікат
                   </Button>
              </div>
-             
-           
              <br/><br/><br/>
 
             {allCourses.length === 0 ? (
-             <div className="rowBlock">
+                  <Col>
            
-           <p > Курс пройдено, сертифікат можна переглянути в  <Link to={urlaim} > <b>Досягненнях</b> </Link> </p><br/>
-        
-
-
-            </div>
+           <p > Курс пройдено, сертифікат можна переглянути в   <div className="Link"  onClick={() => showListCertificates()}> <b>Досягненнях</b> </div>  </p><br/>
+           </Col>
            ) : null}
 
-           
+      <ListOfAchievementsModal
+        userToken={userToken}
+        visibleModal={visibleListModal}
+        setVisibleModal={setvisibleListModal}
+        achievementDoc={achievementDoc}
+        hasAccess={
+          userProfileAccess["CanSeeUserDistinction"] ||
+          userToken.nameid === userId
+        }
+        hasAccessToSeeAndDownload={
+          userProfileAccess["CanDownloadUserDistinction"] ||
+          userToken.nameid === userId
+        }
+        hasAccessToDelete={
+          userProfileAccess["CanDeleteUserDistinction"] ||
+          userToken.nameid === userId
+        }
+        setAchievementDoc={setAchievementDoc}
+      />
+
+
+
+
              <AddAchievementsModal
              userId={activeUserId}
              visibleModal={visibleAchievementModal}
