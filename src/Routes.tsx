743db5afa028e0587fab05c1d97902c124aa1af9
Vladyslav Synieok <55637550+vsynieok@users.noreply.github.com>
2022-08-08 11:17:38 +0300

    fix event route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -64,6 +64,7 @@ import AnnouncementsTable from "./pages/AnnouncementsTable/AnnouncementsTable";
 import RegionBoardMainAdministration from "./pages/RegionsBoard/RegionBoardMainAdministration";
 import DecisionTable from "./pages/DecisionTable/DecisionTable";
 import RegionFollowers from "./pages/Regions/RegionFollowers";
+import EventInfo from "./pages/Actions/ActionEvent/EventInfo/EventInfo";
 
 export default [
   {
@@ -321,7 +322,7 @@ export default [
   {
     path: "/events/details/:id",
     name: "Подія",
-    Component: ActionEvent,
+    Component: EventInfo,
   },
   {
     path: "/userpage/eventUsers/:userId",
