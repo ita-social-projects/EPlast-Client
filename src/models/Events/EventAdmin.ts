ac3217fe1a872e8b244e9fbe187e9be4968cb5b1
Vladyslav Synieok <55637550+vsynieok@users.noreply.github.com>
2022-10-20 09:14:36 +0300

    add back events redesign (#1136)
    * Revert "Revert "Events page redesign (#1042)" (#1087)"
    
    This reverts commit 51142c829fd08a31b61c5519607e84b40ad2549e.
    
    * fix warning style

---
+++ src/models/Events/EventAdmin.ts
@@ -0,0 +1,6 @@
+export interface EventAdmin {
+  userId: string;
+  fullName: string;
+  adminType: string;
+  avatarUrl: string;
+}
--- src/pages/Actions/Actions.tsx
+++ src/pages/Actions/Actions.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import ActionCard from "../ActionCard/ActionCard";
-import eventsApi from "../../api/eventsApi";
 import { Pagination } from "antd";
-import classes from "./Actions.module.css";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import eventsApi from "../../api/eventsApi";
+import ActionCard from "../ActionCard/ActionCard";
 import Spinner from "../Spinner/Spinner";
+import classes from "./Actions.module.css";
 
 const Actions = () => {
   const [loading, setLoading] = useState<boolean>(false);
