c8443cee48bb2e7ae8acb136492a23e7f46ee23b
Mykola123-NMT <mykolanenchuk@gmail.com>
2022-10-27 14:53:18 +0300

    Message modification

--- src/pages/Error/NotFound.tsx
+++ src/pages/Error/NotFound.tsx
@@ -7,7 +7,7 @@ const NotFoundPage = () => {
     <Result
       status="404"
       title="404"
-      subTitle="Вибачте, ця сторінка не існує."
+      subTitle="Вибачте, ця сторінка не існує. Можливо її було видалено іншим користувачем. Перевірте актуальність інформації."
       extra={
         <Button href="/" className={classes.button} style={{ padding: "4px" }}>
           На головну
